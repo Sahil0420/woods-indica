325e81669091e312e9a1ffc7dd1f285b35ce4ba5
Tushar4059x <gulyanitushar@gmail.com>
2024-07-20 17:06:47 +0530

    image

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         "home-hero":
-          "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('./src/assets/forest3.jpg')",
+          "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('./src/assets/workshop.jpg')",
       },
       transitionProperty: {
         width: "width",
